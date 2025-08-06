3c58e295f68077cbada4fd6f48878687cb5c29b2
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-06 07:20:35 +0530

    Simple Mathmatics

--- Array.sh
+++ Array.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 Names=("Manchem" "Viswa" "Nadham" "Lakshmi" "Pavani")
-echo "The First Name:$Names[0]"
+echo "The First Name:${$Names[0]}"
+echo "The Third Name:${$Names[2]}"
+echo "The sixth Name:${$Names[6]}"
+echo "The All Name:${$Names[@]}"
