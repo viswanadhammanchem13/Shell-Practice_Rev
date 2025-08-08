f44dd183ea18532bb568c187c6c47106192463da
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-08 20:08:49 +0530

    Simple Mathmatics

---
+++ Functions_UnInstallation.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+USERID=$(id -u) #Stores User UID
+if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
+then #!= 0 Enter into Loop
+    echo " Error:Please proceed the Installation with sudo" #Prints this messages on Screen
+    exit 1 #!= 0 Don't Proceed with next command and Exit
+else #If =0 Enter into else loop
+    echo "Please proceed the Installation"  #Prints this messages on Screen
+fi #Condition Ends
+
+Validate (){ #Function Definition
+    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
+    then #Enter into Loop
+        echo "$2 Un Installation is suceefull" #Prints this messages on Screen
+    else #Checks If Exit code != Zero, No
+        echo "$2 Un Installation is failed" #Prints this messages on Screen
+        exit 1 #Condition Exits and Entire Script Fails.
+    fi #Condition Ends
+
+}
+
+dnf list installed mysql #Checks whether My SQL Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "MySQL is already Un Installed..." #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "MYSQL is Not installed.." #Prints this messages on Screen
+    dnf remove mysql -y #Un Install Mysql Package
+    Validate $? "mysql" #Calls Validate Function and checks the status
+    
+fi #Condition Ends
+
+dnf list installed nginx #Checks whether Nginx Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "Nginx is already Un Installed..." #Prints this messages on Screen
+    else #Checks If Exit code != Zero, No
+    echo "Nginx is Uninstalled.." #Prints this messages on Screen
+    dnf romove nginx -y #Un Install Nginx Package
+    Validate $? "nginx" #Calls Validate Function and checks the status
+    
+fi #Condition Ends
+
+dnf list installed python3 #Checks whether Python3 Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "Python is Un Installed..." #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "Python is uninstalled Going to install.." #Prints this messages on Screen
+    dnf remove python3 -y #Un Install Python Package
+    Validate $? "nginx" #Calls Validate Function and checks the status
+fi #Condition Ends
+
+dnf list installed nodejs #Checks whether Python3 Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "Node JS is Un Installed..." #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "Node JS is not installed Going to install.." #Prints this messages on Screen
+    dnf remove nodejs -y #un Install Python Package
+    Validate $? "nodejs" #Calls Validate Function and checks the status
+fi #Condition Ends
