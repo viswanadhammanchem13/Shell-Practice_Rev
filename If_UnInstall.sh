bf24445756853d08c1c9b395df5a1ebfb1263386
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-08 18:56:40 +0530

    Simple Mathmatics

---
+++ If_UnInstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo " Error:Please proceed the UnInstallation with sudo"
+    exit 1 #!= 0 Don't Proceed with next command
+else
+    echo "Please proceed the UnInstallation"
+fi
+dnf list installed mysql
+if [ $? -eq 0 ]
+then
+    echo "My SQL is  Installed..."
+    
+else
+    echo "My SQL is not installed.... No issue"
+    exit 1
+    dnf remove mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "UnInstallation is suceefull"
+    else
+        echo "UnInstallation is failed"
+        exit 1
+    fi
+fi
