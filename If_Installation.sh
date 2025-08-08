851c645f4d3cf2bf2f4c1182918a4778881900db
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-08 17:32:02 +0530

    Simple Mathmatics

---
+++ If_Installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo " Error:Please proceed the Installation with sudo"
+    exit 1 #!= 0 Don't Proceed with next command
+else
+    echo "Please proceed the Installation"
+fi
+dnf install mysql -y
+if [ $? -eq 0 ]
+then
+     echo "Installation is suceefull"
+else
+    echo "Installation is failed"
+    #exit 1
+fi
