fa84071758fc57f1a76ced01cf3fdfe358722155
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-08 16:09:15 +0530

    Simple Mathmatics

--- Special_Variables.sh
+++ Special_Variables.sh
@@ -2,7 +2,7 @@
 echo "All Varibales Passed: $@"
 echo "Number of Variables Passed: $#"
 echo "Script Name: $0"
-# echo "Present Working Directory: $PWD"
+echo "Present Working Directory: $PWD"
 # echo  "Home Directory :$HOME"
 # echo "Current User": $USER"
 # echo "Current PID: $$"
