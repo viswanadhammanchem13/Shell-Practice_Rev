7601c7f921a97536050319597bff4ee602dceaa3
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-10 09:41:48 +0530

    Simple Mathmatics

--- Forloop.sh
+++ Forloop.sh
@@ -2,10 +2,10 @@
 for i in {1..100}
 do 
     echo $i
-    # if [ i%2 == 0 ] 
-    # then
-    #     echo " $i"
-    # else
-    #     echo " not even"
-    # fi
+    if (( i % 2 == 0 ))
+    then
+        echo " $i"
+    else
+        echo " not even"
+    fi
 done
