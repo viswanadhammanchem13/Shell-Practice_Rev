1e6180a42470818a4ebe14e43f13041d710aba92
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-10 07:03:07 +0530

    Simple Mathmatics

---
+++ Colors2.s
@@ -0,0 +1,66 @@
+#!/bin/bash
+USERID=$(id -u) #Stores User UID
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
+then #!= 0 Enter into Loop
+    echo "$R Error:Please proceed the Installation with sudo $N" #Prints this messages on Screen
+    exit 1 #!= 0 Don't Proceed with next command and Exit
+else #If =0 Enter into else loop
+    echo "$Y Please proceed the Installation $N"  #Prints this messages on Screen
+fi #Condition Ends
+
+Validate (){ #Function Definition
+    if [ $1 -eq 0 ] #Checks If Exit code equls to Zero, Yes
+    then #Enter into Loop
+        echo "$G $2 Installation is suceefull $N" #Prints this messages on Screen
+    else #Checks If Exit code != Zero, No
+        echo " $R $2 Installation is failed $N" #Prints this messages on Screen
+        exit 1 #Condition Exits and Entire Script Fails.
+    fi #Condition Ends
+
+}
+
+dnf list installed mysql #Checks whether My SQL Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "$Y MySQL is already Installed... $N" #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "$R MYSQL is not installed Going to install.. $N" #Prints this messages on Screen
+    dnf install mysql -y #Install Mysql Package
+    Validate $? "mysql" #Calls Validate Function and checks the status
+    
+fi #Condition Ends
+
+dnf list installed nginx #Checks whether Nginx Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "$Y  Nginx is already Installed... $N" #Prints this messages on Screen
+    else #Checks If Exit code != Zero, No
+    echo "$R Nginx is not installed Going to install.. $N" #Prints this messages on Screen
+    dnf install nginx -y #Install Nginx Package
+    Validate $? "nginx" #Calls Validate Function and checks the status
+    
+fi #Condition Ends
+
+dnf list installed python3 #Checks whether Python3 Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "$Y Python is already Installed... $N" #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "$R Python is not installed Going to install.. $N" #Prints this messages on Screen
+    dnf install python3 -y #Install Python Package
+    Validate $? "nginx" #Calls Validate Function and checks the status
+fi #Condition Ends
+
+dnf list installed nodejs #Checks whether Python3 Package is installed or not
+if [ $? -eq 0 ] #Checks If Exit code equls to Zero, Yes
+then #Enter into Loop
+    echo "$Y Node JS is already Installed... $N" #Prints this messages on Screen
+else #Checks If Exit code != Zero, No
+    echo "$R Node JS is not installed Going to install.. $N" #Prints this messages on Screen
+    dnf install nodejs -y #Install Python Package
+    Validate $? "nodejs" #Calls Validate Function and checks the status
+fi #Condition Ends
